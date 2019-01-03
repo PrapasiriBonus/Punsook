ce1d16057156bc50ff8550aca2b871eed60ce01a
Looksorn <35325953+Looksorn@users.noreply.github.com>
2019-01-03 15:24:55 +0700

    Update function

--- functions/index.js
+++ functions/index.js
@@ -69,15 +69,3 @@ exports.copyPetrolToSheet = functions.database.ref("/testing/pretest").onUpdate(
   // Update data to Google Sheets
   await sheets.spreadsheets.values.update(request, {});
 });
-
-// exports.bigben = functions.https.onRequest((req, res) => {
-//   const hours = (new Date().getHours() % 12) + 1 // London is UTC + 1hr;
-//   res.status(200).send(`<!doctype html>
-//     <head>
-//       <title>Time</title>
-//     </head>
-//     <body>
-//       ${'BONG '.repeat(hours)}
-//     </body>
-//   </html>`);
-// });
