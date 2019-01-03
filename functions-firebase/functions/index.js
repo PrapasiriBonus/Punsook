f3003cd8422c4b0ff51a4cdea7e19aa240024bc5
Looksorn <35325953+Looksorn@users.noreply.github.com>
2019-01-03 14:12:07 +0700

    merge sorn

---
+++ functions-firebase/functions/index.js
@@ -0,0 +1,83 @@
+//const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+"use strict";
+
+const functions = require("firebase-functions");
+
+// Firebase Admin initialization
+var admin = require("firebase-admin");
+var serviceAccount = require("./support-7fdcd-firebase-adminsdk-wz0xt-ca4b35d87e.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://support-7fdcd.firebaseio.com"
+});
+
+// Get Google Sheets instance
+const { google } = require("googleapis");
+const sheets = google.sheets("v4");
+
+// Create JWT
+const jwtClient = new google.auth.JWT({
+  email: serviceAccount.client_email,
+  key: serviceAccount.private_key,
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"] // read and write sheets
+});
+
+// Get data from RTDB
+exports.copyPetrolToSheet = functions.database.ref("/testing/pretest").onUpdate(async change => {
+  let data = change.after.val();
+
+  // Convert JSON to Array following structure below
+  /* 
+  [
+    ['COL-A', 'COL-B'],
+    ['COL-A', 'COL-B']
+  ]
+  */
+  var itemArray = [];
+  var valueArray = [];
+  Object.keys(data).forEach((key, index) => {
+    itemArray.push(key);
+    itemArray.push(data[key]);
+    valueArray[index] = itemArray;
+    itemArray = [];
+  });
+
+  let maxRange = valueArray.length + 1;
+
+  // Do authorization
+  await jwtClient.authorize();
+  
+  // Create Google Sheets request
+  let request = {
+    auth: jwtClient,
+    spreadsheetId: "1OJ1Y63p3_aEs4YNLfsHJ6q_q-4QHAF8gaDprczgtWzE",
+    range: "Pretest!A2:T" + maxRange,
+    valueInputOption: "RAW",
+    requestBody: {
+      values: valueArray
+    }
+  };
+  
+  // Update data to Google Sheets
+  await sheets.spreadsheets.values.update(request, {});
+});
+
+// exports.bigben = functions.https.onRequest((req, res) => {
+//   const hours = (new Date().getHours() % 12) + 1 // London is UTC + 1hr;
+//   res.status(200).send(`<!doctype html>
+//     <head>
+//       <title>Time</title>
+//     </head>
+//     <body>
+//       ${'BONG '.repeat(hours)}
+//     </body>
+//   </html>`);
+// });
